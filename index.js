0a72ba19485c13362fad1e5f0c4e48bf260c6647
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-09-12 19:58:47 -0500

    Support some routes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // @ts-check
 import { chromium } from 'playwright'
-import { readFileSync } from 'node:fs'
+import { readFileSync, writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 
 const launchOptions = {
-  headless: false
+  headless: true
 }
 
-const URL = 'https://books.toscrape.com'
-const unwantedCodes = [500, 404]
+const URL = 'http://69.164.223.208:8086/#/' // http://69.164.223.208:8086/#/
+const unwantedCodes = [400, 403, 404, 500, 502, 504]
 
 ;(async () => {
   const browser = await chromium.launch(launchOptions)
@@ -19,12 +19,16 @@ const unwantedCodes = [500, 404]
   for (const route of routes) {
     const { pathname } = route
 
+    console.log(` [+] ${URL + pathname} ✔`)
+
     const page = await browser.newPage()
     const response = await page.goto(URL + pathname)
 
     const status = response?.status() || 500
+    const notFoundInside = await page.locator('h1').allInnerTexts()
 
-    if (!unwantedCodes.includes(status)) {
+    if (!unwantedCodes.includes(status) && !notFoundInside.includes('Not Found')) {
+      page.waitForTimeout(200)
       const parseFilename = pathname.replace(/\//g, '')
       await page.screenshot({ path: `screenshots/${parseFilename}.png` })
     }
@@ -32,18 +36,23 @@ const unwantedCodes = [500, 404]
     await page.close()
 
     routes[index].status = status
+    routes[index].pathname = URL + pathname
     index++
   }
 
   await browser.close()
-  console.log({ routes })
+
+  const filename = join(process.cwd(), `scan-${Date.now()}.json`)
+  writeFileSync(filename, JSON.stringify(routes), 'utf8')
+
+  console.log('\nResultados guardados 🙌👍')
 })()
 
 function readFileWithRoutes () {
   try {
     const path = join(process.cwd(), 'routes.txt')
     const routes = readFileSync(path, 'utf8')
-    const normalizeRoutes = routes.replace('\r', '').split('\n')
+    const normalizeRoutes = routes.replace(/\r/g, '').split('\n')
     return parserObjectRoute(normalizeRoutes)
   } catch (error) {
     console.log('Error: Archivos con rutas no encontrado')
